f4437e0366adf3e29b86458089234914c063a474
Jesse Averbukh <javerbukh@gmail.com>
2016-11-15 10:29:59 -0500

    Update info_ref_files.py

--- info_ref_files.py
+++ info_ref_files.py
@@ -198,16 +198,20 @@ def read_and_check_valid_params(instrument, file_header):
         else:
             print ("Non-valid paramters (Format (Non-valid value, Header located in)): {}".format(non_valid_params))
 
-def check_required_keys_json(json_file):
-    required_keywords = ["title", "reftype", "pedigree", "author", "telescope", "exp_type",\
-        "instrument", "useafter", "descript", "HISTORY", "msaoper"]
-    if set(required_keywords).issubset(set(json_file)):
+def check_required_keys_json_asdf(file_type, file_header):
+    if file_type == "json":
+        required_keywords = ["title", "reftype", "pedigree", "author", "telescope", "exp_type",\
+            "instrument", "useafter", "description", "HISTORY", "msaoper"]
+    elif file_type == "asdf":
+        required_keywords = ["title", "reftype", "pedigree", "author", "telescope", "exp_type",\
+            "instrument", "useafter", "description", "history"]
+    if set(required_keywords).issubset(set(file_header)):
         print ("All required keys are present")
         return True
     else:
         missing_keys = []
         for key in required_keywords:
-            if key not in json_file:
+            if key not in file_header:
                 missing_keys.append(key)
         print ("Missing keys in file: {}".format(missing_keys))
         return False
@@ -225,7 +229,7 @@ def read_and_check_valid_params_json(instrument, file_header):
 
     new_file_header = {}
     for header in file_header:
-        if header == "descript":
+        if header == "description":
             new_file_header[header[:7].upper()] = file_header[header]
         else:
             new_file_header[header[:8].upper()] = file_header[header]
@@ -269,6 +273,64 @@ def read_and_check_valid_params_json(instrument, file_header):
         else:
             print ("Non-valid paramters (Format (Non-valid value, Header located in)): {}".format(non_valid_params))
 
+def read_and_check_valid_params_asdf(instrument, file_header):
+    """
+    Returns which paramters in the file are invalid, if any
+    """
+    non_valid_params = []
+    file_loc = "/grp/hst/cdbs/tools/jwst/valid_params/" + change_style(instrument) + "_valid_params.csv"
+
+    datetime1 = re.compile("([1][9]|([2][0-1]))\d{2}-([0][0-9]|[1][0-2])-([0-2][0-9]|[3][0-1])T([0-1][0-9]|[2][0-3]):[0-5][0-9]:[0-5][0-9]")
+    datetime2 = re.compile("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
+    inflight_datetime = re.compile("INFLIGHT ([1][9]|([2][0-1]))\d{2}-([0][0-9]|[1][0-2])-([0-2][0-9]|[3][0-1]) ([1][9]|([2][0-1]))\d{2}-([0][0-9]|[1][0-2])-([0-2][0-9]|[3][0-1])")
+
+    required_keywords = ["title", "reftype", "pedigree", "author", "telescope", "exp_type",\
+        "instrument", "useafter", "description", "history"]
+    new_file_header = {}
+    for header in required_keywords:
+        if header == "description":
+            new_file_header[header[:7].upper()] = file_header.tree[header]
+        else:
+            new_file_header[header[:8].upper()] = file_header.tree[header]
+    file_header = new_file_header
+
+    with open(file_loc, 'rb') as csvfile:
+        keyreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in keyreader:
+            if row[0].lower() in file_header or (row[0] == "HISTORY" and row[0] in file_header):
+            #for header in file_header:
+                #Valid value
+                if file_header[row[0]] in row[1:]:
+                    pass
+                #Check USEAFTER
+                elif row[0] == 'USEAFTER':
+                    if re.match(datetime1, file_header[row[0]]):
+                        pass
+                    elif re.match(datetime2, file_header[row[0]]):
+                        print ("Correct format but inaccurate dates in USEAFTER")
+                        non_valid_params.append((file_header[row[0]], row[0]))
+                    else:
+                        non_valid_params.append((file_header[row[0]], row[0]))
+                #Check PEDIGREE
+                elif row[0] == 'PEDIGREE':
+                    valid_options = ['SIMULATION', 'GROUND', 'DUMMY']
+                    if (file_header[row[0]] in valid_options) or re.match(inflight_datetime, file_header[row[0]]):
+                        pass
+                    else:
+                        non_valid_params.append((file_header[row[0]], row[0]))
+                #Check's to see if certain headers are not empty
+                elif row[0] in ['AUTHOR', 'DESCRIP', 'HISTORY']:
+                        if file_header[row[0]] == "":
+                            non_valid_params.append((file_header[row[0]], row[0]))
+                #Not a valid value
+                else:
+                    non_valid_params.append((file_header[row[0]], row[0]))
+            else:
+                pass
+        if not non_valid_params:
+            print ("All parameters are valid")
+        else:
+            print ("Non-valid paramters (Format (Non-valid value, Header located in)): {}".format(non_valid_params))
 ################################################################################
 # Main
 ################################################################################
@@ -289,6 +351,7 @@ for filename in os.listdir(directory):
             hdulist = fits.open(new_path)
         except e:
             print ("NOT A VALID FILE")
+            break
         if check_usability(hdulist):
             instrument_team = hdulist[0].header['INSTRUME']
             check_required_keys(instrument_team, filename, hdulist)
@@ -300,7 +363,8 @@ for filename in os.listdir(directory):
             json_file = json.load(open(new_path))
         except e:
             print ("NOT A VALID FILE")
-        if check_required_keys_json(json_file):
+            break
+        if check_required_keys_json_asdf("json", json_file):
             read_and_check_valid_params_json(json_file["instrument"], json_file)
         print ("------------------------------------------------------------\n")
     elif filename.endswith(".asdf"):
@@ -309,6 +373,9 @@ for filename in os.listdir(directory):
             asdf_file = asdf.open(new_path)
         except e:
             print ("NOT A VALID FILE")
+            break
+        if check_required_keys_json_asdf("asdf", asdf_file.tree):
+            read_and_check_valid_params_asdf(asdf_file.tree["instrument"], asdf_file)
         print ("------------------------------------------------------------\n")
     else:
         continue
